9613c2e9e4dd0fdca7a8fd5c65465af5dbf6ad58
NNHieu <46880398+NNHieu@users.noreply.github.com>
2025-09-01 19:36:54 +0000

    Deploying to gh-pages from @ NNHieu/nnhieu.github.io@39bb2a4a760015a5f186de0a45a4eb4d8a4329b3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,32 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-publications",
           title: "publications",
           description: "For an up-to-date list of my research papers, please see my Google Scholar profile. * denotes equal contribution.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "post-just-know-stuffs",
+        },{id: "post-chuyện",
+        
+          title: "Chuyện",
+        
+        description: "Nơi mình lan man chuyện hàng ngày.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/daily-story/";
+          
+        },
+      },{id: "post-just-know-stuffs",
         
           title: "Just know stuffs",
         
@@ -25,28 +43,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2025/just-know-stuff/";
-          
-        },
-      },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-a-distill-style-blog-post",
-        
-          title: "a distill-style blog post",
-        
-        description: "an example of a distill-style blog post and main elements",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2021/distill/";
           
         },
       },{id: "books-the-godfather",
