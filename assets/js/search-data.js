422e0d28d6a485b24147ac2f1244d7f4071d2b6e
NNHieu <46880398+NNHieu@users.noreply.github.com>
2025-10-02 01:23:45 +0000

    Deploying to gh-pages from @ NNHieu/nnhieu.github.io@1a78153e249791faf5eab13178e5621264d30d3c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -27,7 +27,7 @@ ninja.data = [{
         
           title: "Chuyện",
         
-        description: "Nơi mình lan man chuyện hàng ngày.",
+        description: "Nơi mình lan man chuyện hàng ngày và thể hiện trình độ con mẹ dạy Văn.",
         section: "Posts",
         handler: () => {
           
